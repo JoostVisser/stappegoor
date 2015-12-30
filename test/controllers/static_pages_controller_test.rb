5a0b0c350a2c8cdd528d423764103a865f90975d
JoostVisser <j.w.j.h.visser@student.tue.nl>
2015-12-30 14:01:51 +0100

    Made basic structure for pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,20 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  def setup
+    @base_title = "Horeca Recreatiebad Stappegoor"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", @base_title
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
   end
 
 end
