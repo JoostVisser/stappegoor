c30bc202f5af4ca07dce96fd2de97a20fd1e1979
JoostVisser <j.w.j.h.visser@student.tue.nl>
2016-01-18 06:01:54 -0500

    Temp commit

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,16 @@
 class StaticPagesController < ApplicationController
+
   def home
   end
 
   def kinderfeesten
+    add_breadcrumb "home", root_path
+    add_breadcrumb "kinderfeesten", kinderfeesten_path
   end
 
+
   def contact
+    add_breadcrumb "home", root_path
+    add_breadcrumb "contact", contact_path
   end
 end
