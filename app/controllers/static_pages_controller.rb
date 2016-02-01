39e20172c42f540ff5fae8c44c8ab285c4b439a5
Joost Visser <j.w.j.h.visser@student.tue.nl>
2016-02-01 07:41:39 -0500

    test commit

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,12 +5,10 @@ class StaticPagesController < ApplicationController
 
   def kinderfeesten
     add_breadcrumb "home", root_path
-    add_breadcrumb "kinderfeesten", kinderfeesten_path
   end
 
 
   def contact
     add_breadcrumb "home", root_path
-    add_breadcrumb "contact", contact_path
   end
 end
