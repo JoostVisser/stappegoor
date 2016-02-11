e4d4f43ccf377d3d57855ac7faedd5e6ce70a2e3
JoostVisser <j.w.j.h.visser@student.tue.nl>
2016-02-11 19:28:53 +0100

    New look homepage, start kinderfeest section, add css utility file and change basic colours of website

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,7 +13,7 @@ module ApplicationHelper
   # Link to the website with the text.
   # Adds an active class if current page is linked page.
   def nav_link(link_text, link_path)
-    class_name = current_page?(link_path) ? 'active' : ''
+    class_name = current_page?(link_path) ? 'active' : nil
 
     content_tag(:li, class: class_name) do
       link_to link_text, link_path
@@ -23,7 +23,7 @@ module ApplicationHelper
   # Link to the website with the text.
   # Adds an active class if current page is linked page.
   def breadcrumb_link(link_text, link_path)
-    class_name = current_page?(link_path) ? 'active' : ''
+    class_name = current_page?(link_path) ? 'active' : nil
 
     content_tag(:li, class: class_name) do
       link_to link_text, link_path
