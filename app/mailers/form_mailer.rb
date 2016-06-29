b7874bfbd22ed4c165db8c1aa5173d7009e1d7da
Joost Visser <j.w.j.h.visser@student.tue.nl>
2016-06-29 23:56:58 +0200

    Add minor form_mailer tweaks

--- app/mailers/form_mailer.rb
+++ app/mailers/form_mailer.rb
@@ -2,6 +2,6 @@ class FormMailer < ApplicationMailer
   def sample_email
     @name = "Joost"
     @email = "joost.visser1@gmail.com"
-    mail(to: @email, subject: 'Sample Email')
+    email = mail to: @email, from:'example@email.com' subject: 'Sample Email'
   end
 end
